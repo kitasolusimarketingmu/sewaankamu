cdb6c7b9105e7d270b60d18bb6c306795bcb23e1
kitasolusimarketingmu <118248295+kitasolusimarketingmu@users.noreply.github.com>
2023-06-30 17:44:31 +0700

    Create voice-7dewa.js

---
+++ voice-7dewa.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  var playstatus = 0;
+
+  $('body').click(function(){
+    if(responsiveVoice.isPlaying()) {
+      responsiveVoice.pause();
+
+      console.log('pause speak');
+    }else{
+      if(playstatus == 0){
+         playstatus = 1;
+         var text = document.getElementsByClassName('footer')[0].innerText;
+
+        console.log('start speak');
+        
+        responsiveVoice.setDefaultVoice("Indonesian Female");
+        responsiveVoice.speak(text);
+      }else{
+        responsiveVoice.resume();
+        console.log('resume speak');
+      }
+    }
+  });
+});
