90c46f9e4fae7dcfcba443a69dfb433500e8fb36
kitasolusimarketingmu <118248295+kitasolusimarketingmu@users.noreply.github.com>
2023-06-30 17:27:25 +0700

    Update voice-mitra138.js

--- voice-mitra138.js
+++ voice-mitra138.js
@@ -1,1 +1,19 @@
+$(document).ready(function(){
+  var playstatus = 0;
 
+  $('body').click(function(){
+    if(responsiveVoice.isPlaying()) {
+      responsiveVoice.pause();
+    }else{
+      if(playstatus == 0){
+         playstatus = 1;
+         var text = document.getElementsByClassName('footer')[0].innerText;
+  
+        responsiveVoice.setDefaultVoice("Indonesian Female");
+        responsiveVoice.speak(text);
+      }else{
+        responsiveVoice.resume();
+      }
+    }
+  });
+});
