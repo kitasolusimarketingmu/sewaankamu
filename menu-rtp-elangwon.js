19e58a98ffd62d0f3d4d355292c4af03ff6b2ac0
kitasolusimarketingmu <118248295+kitasolusimarketingmu@users.noreply.github.com>
2023-07-09 14:00:27 +0700

    Create menu-rtp-elangwon.js

---
+++ menu-rtp-elangwon.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+	$('.main-nav').append('<div class="nav-item "> <a class="navlink" href="https://rtpelangwon.com/"> <div class="nav-icon"> <img src="https://i.ibb.co/n7HkMwq/5-Of42-Vh-Wbw-NBmo681-Ugzi0-Aqjpx-FTu7-XJR0-Wc-Iec.webp" alt="5-Of42-Vh-Wbw-NBmo681-Ugzi0-Aqjpx-FTu7-XJR0-Wc-Iec" style="padding-bottom: 7px; padding-top: 9px;"> </div><div class="nav-title" i18n="@RTP">RTP</div></a></div>');
+});
