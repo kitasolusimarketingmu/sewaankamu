e16c81bd663b19d1d27053a810fdcf29637ad77f
kitasolusimarketingmu <118248295+kitasolusimarketingmu@users.noreply.github.com>
2023-07-26 18:23:08 +0700

    Update btn-event-dan-rtp-mitra138.js

--- btn-event-dan-rtp-mitra138.js
+++ btn-event-dan-rtp-mitra138.js
@@ -1,1 +1,3 @@
-$('.login-form').append('<button type="button" class="btn btn-success fw-bold" onclick="window.open(\'https://mitra138rtp.org/\')">RTP SLOT GACOR</button><button type="button" class="btn btn-warning fw-bold" onclick="window.open(\'https://m138.info/?content=promosi\')">EVENT SLOT</button>');
+$(document).ready(function(){
+    $('.login-form').append('<button type="button" class="btn btn-success fw-bold" onclick="window.open(\'https://mitra138rtp.org/\')">RTP SLOT GACOR</button><button type="button" class="btn btn-warning fw-bold" onclick="window.open(\'https://m138.info/?content=promosi\')">EVENT SLOT</button>');
+});
