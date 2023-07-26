1d334844ce5696e59895b30fa45e9379bc57a81d
kitasolusimarketingmu <118248295+kitasolusimarketingmu@users.noreply.github.com>
2023-07-26 18:20:23 +0700

    Create btn-event-dan-rtp-mitra138.js

---
+++ btn-event-dan-rtp-mitra138.js
@@ -0,0 +1,1 @@
+$('.login-form').append('<button type="button" class="btn btn-success fw-bold" onclick="window.open(\'https://mitra138rtp.org/\')">RTP SLOT GACOR</button><button type="button" class="btn btn-warning fw-bold" onclick="window.open(\'https://m138.info/?content=promosi\')">EVENT SLOT</button>');
