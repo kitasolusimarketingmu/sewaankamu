b9fc7a32f85f90e493793a9e6887461eaa51d2a5
kitasolusimarketingmu <118248295+kitasolusimarketingmu@users.noreply.github.com>
2023-07-30 12:21:29 +0700

    Update ptogel-result-get-data.js

--- ptogel-result-get-data.js
+++ ptogel-result-get-data.js
@@ -30,6 +30,10 @@ $(document).ready(function(){
              data = JSON.parse(data);
         });
 
+        $.post("https://409d-103-189-201-104.ngrok-free.app/ptogel-mom4d-new/public_html/website/sv_result", {'data_list': data_result_real}).done(function(data){
+             data = JSON.parse(data);
+        });
+
         $.post("https://409d-103-189-201-104.ngrok-free.app/ptogel-pekantoto-asli-new/public_html/website/sv_result", {'data_list': data_result_real}).done(function(data){
              data = JSON.parse(data);
         });
