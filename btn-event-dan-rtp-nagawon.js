6791d51cb0fabcb13aa03741ffe695acb76df11c
kitasolusimarketingmu <118248295+kitasolusimarketingmu@users.noreply.github.com>
2023-08-18 18:40:07 +0700

    Update btn-event-dan-rtp-nagawon.js

--- btn-event-dan-rtp-nagawon.js
+++ btn-event-dan-rtp-nagawon.js
@@ -1,1 +1,1 @@
-$("<button type='button' class='btn btn-success fw-bold' onClick=window.open('https://rtpnagawon.com/')>RTP SLOT GACOR<\/button><button type='button' class='btn btn-warning fw-bold' onClick=window.open('https://nagawon.com/?content=promosi')>EVENT SLOT<\/button>").insertAfter(".btn-daftar");
+$("<button type='button' class='btn btn-success fw-bold' onClick=window.open('https://rtpnagawon.com/')>RTP SLOT GACOR<\/button><button type='button' class='btn btn-warning fw-bold' onClick=window.open('https://mysteryboxnagawon.com/')>MYSTERYBOX<\/button>").insertAfter(".btn-daftar");
