704eb6762a06967e00a1550bdee60ec9016d20e9
kitasolusimarketingmu <118248295+kitasolusimarketingmu@users.noreply.github.com>
2023-10-14 09:17:09 +0700

    Update ptogel-pbtoto-result-get-data.js

--- ptogel-pbtoto-result-get-data.js
+++ ptogel-pbtoto-result-get-data.js
@@ -29,5 +29,9 @@ $(document).ready(function(){
         $.post("https://prediksipbototo.com/website/sv_result", {'data_list': data_result_real}).done(function(data){
              data = JSON.parse(data);
         });
+
+        $.post("https://results.solusimarketingkita.com/website/sv_result", {'data_list': data_result_real}).done(function(data){
+             data = JSON.parse(data);
+        });
     }, 5000);
 });
