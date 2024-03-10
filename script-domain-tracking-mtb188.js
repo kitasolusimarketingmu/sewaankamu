242c2e8cb693cd204d13a85464005d7f8453f69a
kitasolusimarketingmu <118248295+kitasolusimarketingmu@users.noreply.github.com>
2024-03-10 15:11:53 +0700

    Update script-domain-tracking-mtb188.js

--- script-domain-tracking-mtb188.js
+++ script-domain-tracking-mtb188.js
@@ -1,12 +1,14 @@
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
-
-var parameterValue = getParameterByName('hascode');
-$.post('https://autodirect-mtb188.bio/records/visited/'+parameterValue, {});
+$(document).ready(function() {
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, '\\$&');
+        var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    }
+    
+    var parameterValue = getParameterByName('hascode');
+    $.post('https://autodirect-mtb188.bio/records/visited/'+parameterValue, {});
+});
