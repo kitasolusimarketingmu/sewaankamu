e33668312be1155d74c008fcb27f603adf385e71
kitasolusimarketingmu <118248295+kitasolusimarketingmu@users.noreply.github.com>
2025-01-15 23:25:03 +0700

    Update untuk-promo-hamtoto.js

--- untuk-promo-hamtoto.js
+++ untuk-promo-hamtoto.js
@@ -1,35 +1,39 @@
-function isPromotionPage() {
-	return window.location.href.indexOf("promotion") > -1;
-}
+    function isPromotionPage() {
+        return window.location.href.indexOf("promotion") > -1;
+    }
 
-function isDesktop() {
-	return window.innerWidth >= 1024;
-}
+    function isDesktop() {
+        return window.innerWidth >= 1024;
+    }
 
+    document.addEventListener("DOMContentLoaded", function() {
+    	console.log('Masoook pak eko');
+    	setTimeout(function() {
+	        if (isPromotionPage() && isDesktop()) {
+	        	var style = document.createElement("style");
+			    style.type = "text/css";
+			    style.innerHTML = `
+			        .wimage {
+			            border-radius: 0px;
+			        }
 
-if (isPromotionPage() && isDesktop()) {
-	var style = document.createElement("style");
-	    style.type = "text/css";
-	    style.innerHTML = `
-		.wimage {
-		    border-radius: 0px;
-		}
+			        .panel-heading {
+			            padding: 0px 15px;
+			            margin-top: -1px;
+			        }
+			    `;
+			    document.body.appendChild(style);
 
-		.panel-heading {
-		    padding: 0px 15px;
-		    margin-top: -1px;
-		}
-	    `;
-	    document.body.appendChild(style);
+	            var kotakkan = document.querySelectorAll('#content')[0];
+				var centerElements = document.querySelectorAll("center");
+				centerElements.forEach(function(element) {
+				    element.remove();
+				});
 
-    var kotakkan = document.querySelectorAll('#content')[0];
-		var centerElements = document.querySelectorAll("center");
-		centerElements.forEach(function(element) {
-		    element.remove();
-		});
-
-		var brElements = document.querySelectorAll("br");
-		brElements.forEach(function(element) {
-		    element.remove();
-		});
-}
+				var brElements = document.querySelectorAll("br");
+				brElements.forEach(function(element) {
+				    element.remove();
+				});
+	        }
+    	});
+    });
