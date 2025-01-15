103dfcddf62a106ab2340831b4d286e25c467cda
kitasolusimarketingmu <118248295+kitasolusimarketingmu@users.noreply.github.com>
2025-01-16 00:29:52 +0700

    Update untuk-promo-pekantoto.js

--- untuk-promo-pekantoto.js
+++ untuk-promo-pekantoto.js
@@ -95,15 +95,15 @@
 					    text-shadow: 2px 2px 0px rgb(0 0 0) !important;
 					}
 					.promosi-content-container .promosi-content .promosi-btn-wrapper .btn-promo#btn-promo-more {
-					    background: linear-gradient(to bottom, #fdef02 0%, #ffae00 100%) !important;
+					    background: linear-gradient(to bottom, #4ae7fd 0%, #008bd0 100%) !important;
 					    color: #000 !important;
 					    width: 100% !important;
 					    height: 76px !important;
-					    box-shadow: inset 0 0 5px 3px #f18700, inset 0 2px 0 0 #fff, inset 0 0 0 2px #640067, 3px 3px 3px 1px rgba(0, 0, 0, .2);
+					    box-shadow: inset 0 0 5px 3px #0084bb, inset 0 2px 0 0 #fff, inset 0 0 0 2px #4be9ff, 3px 3px 3px 1px rgba(0, 0, 0, .2);
 					    border-radius: 0px 5px 5px 0px !important;
 					    margin-top: 0px !important;
 					    padding: 2px !important;
-					    border: 1px solid #fdef02;
+					    border: 1px solid #0d6485;
 					}
 					.promosi-content-container .promosi-content .promosi-btn-wrapper {
 					        width: 14%!important;
