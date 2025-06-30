c2cffc5f8bbcded25cce5fe5a3dd9b0660106a6c
kitasolusimarketingmu <118248295+kitasolusimarketingmu@users.noreply.github.com>
2025-06-30 20:09:26 +0700

    Update add-gamenew-son4d.js

--- add-gamenew-son4d.js
+++ add-gamenew-son4d.js
@@ -167,54 +167,98 @@ window.addEventListener('DOMContentLoaded', function() {
 	var ppslot = document.getElementById('ppslot');
 	var pgslot = document.getElementById('pgslot')
 
-	if (isUserArea) {
+	setTimeout(function(){
+		if (isUserArea) {
+			if (ppslot != undefined) {
+				const elements = document.querySelectorAll("*");
+				let MahjongWins3 = null;
+				let MahjongWins2 = null;
 
-		if (ppslot != undefined) {
-			const htmlString = `
-				<div style="height:190px; width:170px;display:inline-block; text-align:center; vertical-align:top" bis_skin_checked="1">
-				   <a href="" bis_size="{&quot;x&quot;:231,&quot;y&quot;:694,&quot;w&quot;:170,&quot;h&quot;:103,&quot;abs_x&quot;:231,&quot;abs_y&quot;:694}">
-				      <img src="https://imggalery.com/uploads/user_61/album_87/1748777520.gif" style="width:150px" bis_size="{&quot;x&quot;:241,&quot;y&quot;:627,&quot;w&quot;:150,&quot;h&quot;:150,&quot;abs_x&quot;:241,&quot;abs_y&quot;:627}" bis_id="bn_fz6hanbyrvksfmmz61w431">
-				      <div style="text-align:center; margin-top:3px;" bis_skin_checked="1">Mahjong Wins 2</div>
-				   </a>
-				</div>
-				<div style="height:190px; width:170px;display:inline-block; text-align:center; vertical-align:top" bis_skin_checked="1">
-				   <a href="" bis_size="{&quot;x&quot;:61,&quot;y&quot;:694,&quot;w&quot;:170,&quot;h&quot;:103,&quot;abs_x&quot;:61,&quot;abs_y&quot;:694}">
-				      <img src="https://www.kliker.store/img/gifmee.gif" style="width:150px" bis_size="{&quot;x&quot;:71,&quot;y&quot;:627,&quot;w&quot;:150,&quot;h&quot;:150,&quot;abs_x&quot;:71,&quot;abs_y&quot;:627}" bis_id="bn_lbg8qcmgu2yu3k1z2u1eo7">
-				      <div style="text-align:center; margin-top:3px;" bis_skin_checked="1">Mahjong Wins 3 - Black Scatter</div>
-				   </a>
-				</div>
-			`;
+				elements.forEach(el => {
+				  if (el.textContent.trim().includes("Mahjong Wins 3 - Black Scatter")) {
+				    const parent = el.closest("a");
+				    if (parent && parent.href) {
+				      MahjongWins3 = parent.href;
+				    }
+				  }
+				});
 
-			const tempDiv = document.createElement('div');
-		    tempDiv.innerHTML = htmlString.trim();
-		    const newGameItem1 = tempDiv.children[1];
-		    const newGameItem0 = tempDiv.children[0];
-		    if(ppslot != undefined) ppslot.insertBefore(newGameItem1, ppslot.firstChild);
-		    if(ppslot != undefined) ppslot.insertBefore(newGameItem0, ppslot.firstChild);
+				elements.forEach(el => {
+				  if (el.textContent.trim().includes("Mahjong Wins 2")) {
+				    const parent = el.closest("a");
+				    if (parent && parent.href) {
+				      MahjongWins2 = parent.href;
+				    }
+				  }
+				});
 
-		}else if (pgslot != undefined) {
-			const htmlString = `
-				<div style="height:200px; width:170px; display:inline-block; text-align:center; vertical-align:top" bis_skin_checked="1">
-				   <a href="" bis_size="{&quot;x&quot;:58,&quot;y&quot;:1099,&quot;w&quot;:170,&quot;h&quot;:110,&quot;abs_x&quot;:58,&quot;abs_y&quot;:1099}">
-				      <img src="https://imggalery.com/uploads/user_61/album_87/1748774220_1.webp" style="width:160px; cursor: pointer;" bis_size="{&quot;x&quot;:63,&quot;y&quot;:1027,&quot;w&quot;:160,&quot;h&quot;:160,&quot;abs_x&quot;:63,&quot;abs_y&quot;:1027}" bis_id="bn_mfeekz7dx4szgfbtmxbrir">
-				      <div style="text-align:center; margin-top:5px" bis_skin_checked="1">Mahjong Ways</div>
-				   </a>
-				</div>
-				<div style="height:200px; width:170px; display:inline-block; text-align:center; vertical-align:top" bis_skin_checked="1">
-				   <a href="" bis_size="{&quot;x&quot;:231,&quot;y&quot;:1099,&quot;w&quot;:170,&quot;h&quot;:110,&quot;abs_x&quot;:231,&quot;abs_y&quot;:1099}">
-				      <img src="https://imggalery.com/uploads/user_61/album_87/1748774220.webp" style="width:160px; cursor: pointer;" bis_size="{&quot;x&quot;:236,&quot;y&quot;:1027,&quot;w&quot;:160,&quot;h&quot;:160,&quot;abs_x&quot;:236,&quot;abs_y&quot;:1027}" bis_id="bn_80p2u39alhwmjycxg6wdar">
-				      <div style="text-align:center; margin-top:5px" bis_skin_checked="1">Mahjong Ways 2</div>
-				   </a>
-				</div>
-			`;
+				const htmlString = `
+					<div style="height:190px; width:170px;display:inline-block; text-align:center; vertical-align:top" bis_skin_checked="1">
+					   <a href="`+MahjongWins2+`" bis_size="{&quot;x&quot;:231,&quot;y&quot;:694,&quot;w&quot;:170,&quot;h&quot;:103,&quot;abs_x&quot;:231,&quot;abs_y&quot;:694}">
+					      <img src="https://imggalery.com/uploads/user_61/album_87/1748777520.gif" style="width:150px" bis_size="{&quot;x&quot;:241,&quot;y&quot;:627,&quot;w&quot;:150,&quot;h&quot;:150,&quot;abs_x&quot;:241,&quot;abs_y&quot;:627}" bis_id="bn_fz6hanbyrvksfmmz61w431">
+					      <div style="text-align:center; margin-top:3px;" bis_skin_checked="1">Mahjong Wins 2</div>
+					   </a>
+					</div>
+					<div style="height:190px; width:170px;display:inline-block; text-align:center; vertical-align:top" bis_skin_checked="1">
+					   <a href="`+MahjongWins3+`" bis_size="{&quot;x&quot;:61,&quot;y&quot;:694,&quot;w&quot;:170,&quot;h&quot;:103,&quot;abs_x&quot;:61,&quot;abs_y&quot;:694}">
+					      <img src="https://www.kliker.store/img/gifmee.gif" style="width:150px" bis_size="{&quot;x&quot;:71,&quot;y&quot;:627,&quot;w&quot;:150,&quot;h&quot;:150,&quot;abs_x&quot;:71,&quot;abs_y&quot;:627}" bis_id="bn_lbg8qcmgu2yu3k1z2u1eo7">
+					      <div style="text-align:center; margin-top:3px;" bis_skin_checked="1">Mahjong Wins 3 - Black Scatter</div>
+					   </a>
+					</div>
+				`;
 
-			const tempDiv = document.createElement('div');
-		    tempDiv.innerHTML = htmlString.trim();
-		    const newGameItem1 = tempDiv.children[1];
-		    const newGameItem0 = tempDiv.children[0];
-		    if(pgslot != undefined) pgslot.insertBefore(newGameItem1, pgslot.firstChild);
-		    if(pgslot != undefined) pgslot.insertBefore(newGameItem0, pgslot.firstChild);
+				const tempDiv = document.createElement('div');
+			    tempDiv.innerHTML = htmlString.trim();
+			    const newGameItem1 = tempDiv.children[1];
+			    const newGameItem0 = tempDiv.children[0];
+			    if(ppslot != undefined) ppslot.insertBefore(newGameItem1, ppslot.firstChild);
+			    if(ppslot != undefined) ppslot.insertBefore(newGameItem0, ppslot.firstChild);
+			}else if (pgslot != undefined) {
+				const elements = document.querySelectorAll("*");
+				let MahjongWays1 = null;
+				let MahjongWays2 = null;
 
+				elements.forEach(el => {
+				  if (el.textContent.trim().includes("Mahjong Ways")) {
+				    const parent = el.closest("a");
+				    if (parent && parent.href) {
+				      MahjongWays1 = parent.href;
+				    }
+				  }
+				});
+
+				elements.forEach(el => {
+				  if (el.textContent.trim().includes("Mahjong Ways 2")) {
+				    const parent = el.closest("a");
+				    if (parent && parent.href) {
+				      MahjongWays2 = parent.href;
+				    }
+				  }
+				});
+
+				const htmlString = `
+					<div style="height:200px; width:170px; display:inline-block; text-align:center; vertical-align:top" bis_skin_checked="1">
+					   <a href="`+MahjongWays1+`" bis_size="{&quot;x&quot;:58,&quot;y&quot;:1099,&quot;w&quot;:170,&quot;h&quot;:110,&quot;abs_x&quot;:58,&quot;abs_y&quot;:1099}">
+					      <img src="https://imggalery.com/uploads/user_61/album_87/1748774220_1.webp" style="width:160px; cursor: pointer;" bis_size="{&quot;x&quot;:63,&quot;y&quot;:1027,&quot;w&quot;:160,&quot;h&quot;:160,&quot;abs_x&quot;:63,&quot;abs_y&quot;:1027}" bis_id="bn_mfeekz7dx4szgfbtmxbrir">
+					      <div style="text-align:center; margin-top:5px" bis_skin_checked="1">Mahjong Ways</div>
+					   </a>
+					</div>
+					<div style="height:200px; width:170px; display:inline-block; text-align:center; vertical-align:top" bis_skin_checked="1">
+					   <a href="`+MahjongWays2+`" bis_size="{&quot;x&quot;:231,&quot;y&quot;:1099,&quot;w&quot;:170,&quot;h&quot;:110,&quot;abs_x&quot;:231,&quot;abs_y&quot;:1099}">
+					      <img src="https://imggalery.com/uploads/user_61/album_87/1748774220.webp" style="width:160px; cursor: pointer;" bis_size="{&quot;x&quot;:236,&quot;y&quot;:1027,&quot;w&quot;:160,&quot;h&quot;:160,&quot;abs_x&quot;:236,&quot;abs_y&quot;:1027}" bis_id="bn_80p2u39alhwmjycxg6wdar">
+					      <div style="text-align:center; margin-top:5px" bis_skin_checked="1">Mahjong Ways 2</div>
+					   </a>
+					</div>
+				`;
+
+				const tempDiv = document.createElement('div');
+			    tempDiv.innerHTML = htmlString.trim();
+			    const newGameItem1 = tempDiv.children[1];
+			    const newGameItem0 = tempDiv.children[0];
+			    if(pgslot != undefined) pgslot.insertBefore(newGameItem1, pgslot.firstChild);
+			    if(pgslot != undefined) pgslot.insertBefore(newGameItem0, pgslot.firstChild);
+
+			}
 		}
-	}
+	}, 2000);
 });
