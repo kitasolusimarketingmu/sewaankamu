72187b30282a835dafdc2554561a6395cb46b5c4
kitasolusimarketingmu <118248295+kitasolusimarketingmu@users.noreply.github.com>
2025-08-08 14:23:07 +0700

    Update untuk-promo-hamtoto.js

--- untuk-promo-hamtoto.js
+++ untuk-promo-hamtoto.js
@@ -119,15 +119,15 @@ function addElementAtIndex(containerId, newElement, index) {
 						    text-shadow: 2px 2px 0px rgb(0 0 0) !important;
 						}
 						.promosi-content-container .promosi-content .promosi-btn-wrapper .btn-promo#btn-promo-more {
-						        background-image: linear-gradient(to bottom, #fae27b 0%, #c6b200 100%) !important;
-								color: #000 !important;
-								width: 100% !important;
-								height: 76px !important;
-								box-shadow: inset 0 0 5px 3px #edd44d, inset 0 2px 0 0 #fff, inset 0 0 0 2px #eddc4c, 3px 3px 3px 1px rgba(0, 0, 0, .2);
-								border-radius: 0px 5px 5px 0px !important;
-								margin-top: 0px !important;
-								padding: 2px !important;
-								border: 1px solid #f7f72a;
+						        background: linear-gradient(to bottom, #fdef02 0%, #ffae00 100%) !important;
+							    color: #000 !important;
+							    width: 100% !important;
+							    height: 76px !important;
+							    box-shadow: inset 0 0 5px 3px #f18700, inset 0 2px 0 0 #fff, inset 0 0 0 2px #640067, 3px 3px 3px 1px rgba(0, 0, 0, .2);
+							    border-radius: 0px 5px 5px 0px !important;
+							    margin-top: 0px !important;
+							    padding: 2px !important;
+							    border: 1px solid #fdef02;
 						}
 						.promosi-content-container .promosi-content .promosi-btn-wrapper {
 						        width: 14%!important;
@@ -165,15 +165,15 @@ function addElementAtIndex(containerId, newElement, index) {
 						    text-shadow: 2px 2px 0px rgb(0 0 0) !important;
 						}
 						.promosi-content-container .promosi-content .promosi-btn-wrapper .btn-promo#btn-promo-more {
-						    	background-image: linear-gradient(to bottom, #fae27b 0%, #c6b200 100%) !important;
-								color: #000 !important;
-								width: 100% !important;
-								height: 76px !important;
-								box-shadow: inset 0 0 5px 3px #edd44d, inset 0 2px 0 0 #fff, inset 0 0 0 2px #eddc4c, 3px 3px 3px 1px rgba(0, 0, 0, .2);
-								border-radius: 0px 5px 5px 0px !important;
-								margin-top: 0px !important;
-								padding: 2px !important;
-								border: 1px solid #f7f72a;
+						    	background: linear-gradient(to bottom, #fdef02 0%, #ffae00 100%) !important;
+							    color: #000 !important;
+							    width: 100% !important;
+							    height: 76px !important;
+							    box-shadow: inset 0 0 5px 3px #f18700, inset 0 2px 0 0 #fff, inset 0 0 0 2px #640067, 3px 3px 3px 1px rgba(0, 0, 0, .2);
+							    border-radius: 0px 5px 5px 0px !important;
+							    margin-top: 0px !important;
+							    padding: 2px !important;
+							    border: 1px solid #fdef02;
 						}
 						.promosi-content-container .promosi-content .promosi-btn-wrapper {
 						        width: 14%!important;
