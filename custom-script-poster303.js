aff92af69676fac391c37ef80d7407d7e886ed6c
kitasolusimarketingmu <118248295+kitasolusimarketingmu@users.noreply.github.com>
2026-06-05 03:09:38 +0700

    Update custom-script-poster303.js

--- custom-script-poster303.js
+++ custom-script-poster303.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const isMobile = /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent);
 
     if (isMobile) {
-        //document.querySelector('body').style.setProperty('background', 'url("https://embedin.site/media/premium/570/mobile.jpeg?v=1f35d61e09ee8fb7")', 'important');
+        document.querySelector('body').style.setProperty('background', 'url("https://embedin.site/media/premium/570/mobile.jpeg?v=1f35d61e09ee8fb7")', 'important');
     }else{
-        //document.querySelector('body').style.setProperty('background', 'url("https://embedin.site/media/premium/568/whatsapp-image-2026-06-04-at-222310.jpeg?v=ff50422a4fcb7452")', 'important');
+        document.querySelector('body').style.setProperty('background', 'url("https://embedin.site/media/premium/568/whatsapp-image-2026-06-04-at-222310.jpeg?v=ff50422a4fcb7452")', 'important');
     }
 
 
@@ -21,4 +21,69 @@ document.addEventListener('DOMContentLoaded', function () {
     })();
     
     $('<div class=poster303-wrap><div class=poster303-title>CARI POSTER303 DISINI <svg aria-hidden=true class="dark:text-white h-6 text-gray-800 w-6"fill=none height=24 viewBox="0 0 24 24"width=24 xmlns=http://www.w3.org/2000/svg><path d="m8 7 4 4 4-4m-8 6 4 4 4-4"stroke=currentColor stroke-linecap=round stroke-linejoin=round stroke-width=2 /></svg></div><div class=poster303-search-strip><div class=poster303-search-icon aria-hidden=true><img alt="search POSTER303"height=35 src=https://photoku.io/images/2025/09/15/cari.png srcset="search POSTER303"width=35></div><a class=poster303-search-pill href=https://www.carikita.id rel="nofollow noopener"target=_blank>www.carikita.id</a></div></div>').insertAfter('.jackpot-content');
+
+
+    const style2 = document.createElement("style");
+    style2.textContent = `
+        #slide-left1{position:fixed;top:110px;left:-180px;width:180px;min-height:180px;z-index:99999;text-align:center;border-radius:0 12px 32px 0;border:1px solid gold;background:linear-gradient(149deg,#000000 -5%,#610000 27%,#460000 39%,rgb(129 0 0) 63%,#480000 80%,#2f0000 98%);transition:left .4s ease-in-out}#toggle-left1{position:absolute;left:180px;top:0;cursor:pointer;z-index:99999}.card-toggle{width:180px;padding:10px 0;display:flex;flex-direction:column;align-items:center;background:#232434}.card-img{width:150px;height:auto;margin-bottom:5px;margin-left:20px}.hours{margin:20px}.img-kontak{width:50px;margin:10px;padding-bottom:9px;border-bottom:5px double gold;transition:.3s}.img-kontak:hover{border-bottom:5px double red}.daftar-btn-bawah{display:grid;grid-template-columns:repeat(2,1fr);gap:0}
+    `;
+    document.head.appendChild(style2);
+
+    // HTML
+    const widget = document.createElement("div");
+    widget.id = "slide-left1";
+
+    widget.innerHTML = `
+        <div id="toggle-left1">
+            <img src="https://embedin.site/media/assets-hubungi-kami/572/hubungikami.gif?v=e22dbf05555c6759" alt="Hubungi Kami">
+        </div>
+
+        <div class="card-toggle">
+            <div class="hours">
+                <a href="/" target="_self">
+                    <img class="card-img"
+                    src="https://embedin.site/media/assets-hubungi-kami/573/icon-24-jam-png.png?v=f726f6edc1579ed9"
+                    alt="24 Jam">
+                </a>
+            </div>
+
+            <div class="daftar-btn-bawah">
+                <a href="https://arahin.store/igposter303" target="_blank" rel="nofollow">
+                    <img class="img-kontak"
+                    src="https://embedin.site/media/assets-hubungi-kami/574/ig.png?v=4ecac73feb456ef9"
+                    alt="Instagram">
+                </a>
+
+                <a href="https://arahin.store/waposter303" target="_blank" rel="nofollow">
+                    <img class="img-kontak"
+                    src="https://embedin.site/media/assets-hubungi-kami/576/wa.png?v=29ed16cb12c5f9ba"
+                    alt="WhatsApp">
+                </a>
+
+                <a href="https://arahin.store/fbposter303" target="_blank" rel="nofollow">
+                    <img class="img-kontak"
+                    src="https://embedin.site/media/assets-hubungi-kami/571/fb.png?v=b274d945e324e7f6"
+                    alt="Facebook">
+                </a>
+
+                <a href="https://arahin.store/teleposter303" target="_blank" rel="nofollow">
+                    <img class="img-kontak"
+                    src="https://embedin.site/media/assets-hubungi-kami/575/tele.png?v=898b6c243afba750"
+                    alt="Telegram">
+                </a>
+            </div>
+        </div>
+    `;
+
+    document.body.appendChild(widget);
+
+    // Toggle
+    const toggle = document.getElementById("toggle-left1");
+    let open = false;
+
+    toggle.addEventListener("click", function () {
+        open = !open;
+        widget.style.left = open ? "0px" : "-180px";
+    });
+
 });
